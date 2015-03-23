3fbd5de4874f5bfe69b33576762e7eb43e0093ea
toran billups <toranb@gmail.com>
2015-03-22 23:50:26 -0500

    [cleanup] removed the not needed child route

--- app/routes/todos/todo.js
+++
@@ -1,8 +0,0 @@
-import Ember from "ember";
-
-export default Ember.Route.extend({
-    model: function(params) {
-        var store = this.get("store");
-        return store.find("todo", params.todo_id);
-    }
-});
