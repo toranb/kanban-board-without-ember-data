c79a5224549fee325dd11f16586eb1e4646df315
toran billups <toranb@gmail.com>
2015-04-18 13:15:54 -0500

    trying to do a non singleton controller

--- app/initializers/repositories.js
+++ app/initializers/repositories.js
@@ -1,8 +1,10 @@
 import registerWithContainer from "ember-cli-auto-register/register";
+import Wat from "kanban/controllers/todos/todo";
 
 export function initialize(container, application) {
     registerWithContainer("repositories", application);
     application.inject("repositories", "store", "store:main");
+    application.register('controller:todos/todo', Wat, {singleton: true });
 }
 
 export default {
